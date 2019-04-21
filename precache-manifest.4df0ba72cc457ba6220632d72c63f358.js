0ce5b4fb77d73dc000b2b42e5c4cbe03a5f9a1d9
Sebastian Coronel <jsebastianck@gmail.com>
2019-04-20 22:06:59 -0300

    Updates

---
+++ precache-manifest.4df0ba72cc457ba6220632d72c63f358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "ff4a7d10b342b68a10e0",
+    "url": "/portfolio/static/js/main.8241ec22.chunk.js"
+  },
+  {
+    "revision": "ee2951b8af5713ecda30",
+    "url": "/portfolio/static/js/2.96b755ad.chunk.js"
+  },
+  {
+    "revision": "ff4a7d10b342b68a10e0",
+    "url": "/portfolio/static/css/main.4f17bd37.chunk.css"
+  },
+  {
+    "revision": "ee2951b8af5713ecda30",
+    "url": "/portfolio/static/css/2.56aa5090.chunk.css"
+  },
+  {
+    "revision": "ee8d3770bfdbb893f68c02105ac9f15d",
+    "url": "/portfolio/index.html"
+  }
+];
